2bdf83d92b77eb589400f458c5fa73899ef421b1
51348761z <hl_wong@st.gxu.edu.cn>
2026-02-10 10:29:09 +0800

    feat: add NotFound component for handling 404 errors in routing

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,6 +3,7 @@ import { TanStackDevtools } from "@tanstack/react-devtools";
 import { QueryClient } from "@tanstack/react-query";
 import {
   HeadContent,
+  Link,
   Outlet,
   createRootRouteWithContext,
 } from "@tanstack/react-router";
@@ -24,6 +25,7 @@ export const Route = createRootRouteWithContext<RouterContext>()({
   }),
 
   component: RootLayout,
+  notFoundComponent: NotFound,
 });
 
 function RootLayout() {
@@ -47,6 +49,23 @@ function RootLayout() {
           },
         ]}
       />
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center py-20 text-center">
+      <h1 className="mb-4 text-4xl font-bold text-gray-800">404</h1>
+      <p className="mb-6 to-gray-600 text-lg">
+        Ooops! The page you are looking for does not exist.
+      </p>
+      <Link
+        to="/"
+        className="rounded-md bg-blue-600 px-6 py-2 text-white transition hover:bg-blue-700"
+      >
+        Go back home
+      </Link>
     </div>
   );
 }
